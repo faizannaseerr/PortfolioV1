bc7af06f000b7bde214ed5bc1e75d9cb2b43c515
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2023-11-10 16:21:22 +0000

    Update delay.tsx

--- components/delay.tsx
+++ components/delay.tsx
@@ -4,6 +4,8 @@ import React, {useState, useEffect} from 'react'
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import Header from './header';
 
+// animate-extend-slow-2  animate-extend-slow-1
+
 export default function Delay({children} : {children: React.ReactNode}) {
   const [showComponent, setShowComponent] = useState(false); 
  
@@ -25,8 +27,8 @@ export default function Delay({children} : {children: React.ReactNode}) {
 const DelayedComponent = ({children} : {children: React.ReactNode}) => {
   return (
     <div className='z-10'>
-      <div className='bg-[#d8f7f6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] animate-extend-slow-2'></div>
-      <div className='bg-[#c2e0f4] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] animate-extend-slow-1'></div> 
+      <div className='bg-[#d8f7f6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+      <div className='bg-[#c2e0f4] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div> 
       <ActiveSectionContextProvider>
         <Header/>
         {children} 
