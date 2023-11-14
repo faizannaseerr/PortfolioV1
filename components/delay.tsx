d0892013c01be764803ebfef8432263c43b03ae4
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2023-11-14 15:30:49 +0000

    Update delay.tsx

--- components/delay.tsx
+++ components/delay.tsx
@@ -28,8 +28,8 @@ export default function Delay({children} : {children: React.ReactNode}) {
 const DelayedComponent = ({children} : {children: React.ReactNode}) => {
   return (
     <div className='z-10'>
-      <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay:0.5}} className='bg-gradient-to-r from-[#d8f7f6] to-[#c2e0f4] absolute top-[-4rem] -z-10 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-3xl sm:w-[68.75rem] animate-extend-slow-1'></motion.div>
-      <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay:0.8}} className='bg-gradient-to-r to-[#d8f7f6] from-[#c2e0f4] absolute top-[-4rem] -z-10 left-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-3xl sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] animate-extend-slow-2'></motion.div> 
+      <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay:0.8}} className='bg-gradient-to-r from-[#d8f7f6] to-[#c2e0f4] absolute top-[-4rem] -z-10 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-3xl sm:w-[68.75rem] animate-extend-slow-1'></motion.div>
+      <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay:0.5}} className='bg-gradient-to-r to-[#d8f7f6] from-[#c2e0f4] absolute top-[-4rem] -z-10 left-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-3xl sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] animate-extend-slow-2'></motion.div> 
       <ActiveSectionContextProvider>
         <Header/>
         {children} 
