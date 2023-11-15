c813e8dd17abfeb41eb477283159c922fadc3b3c
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2023-11-15 02:43:00 +0000

    Update section-divider.tsx

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -7,7 +7,7 @@ export default function SectionDivider() {
   return (
     <div className='flex flex-row'>
       {/* <motion.div className='bg-gray-200 mt-32 mb-32 h-1 w-[45rem] rounded-full hidden sm:block' initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.125}}></motion.div> */}
-      <motion.div className='bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block mx-5' initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.5}}></motion.div>
+      <motion.div className='bg-gray-200 my-16 h-16 w-1 rounded-full hidden sm:block mx-5' initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.5}}></motion.div>
       {/* <motion.div className='bg-gray-200 mt-32 mb-32 h-1 w-[45rem] rounded-full hidden sm:block' initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.125}}></motion.div> */}
     </div>
     
