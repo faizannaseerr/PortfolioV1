7d73943e1d6469f4bd71cab6374bb382f6f0371e
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2024-04-30 01:54:35 -0400

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -21,7 +21,7 @@ export default function About() {
       <SectionHeading> About Me </SectionHeading>
         <p className='sm:mb-3 mb-6'>The intersection of art and technology has always held a profound fascination for me, owing to its unique connections. As a computer science student specializing in software engineering, my primary goal has been to approach software development from a perspective that marries aesthetics and functionality seamlessly. I firmly believe that design plays an integral role in crafting a user experience that is not only coherent but also augments everyday usability.</p>
         <p className='sm:mb-3 mb-6'>In my projects, I strive for an aesthetic simplicity that does not compromise on the value they bring to the end user's daily life. More recently, I've ventured into the realm of incorporating AI into my creations to enhance user comfort. </p>
-        <p> When I'm not immersed in coding, you'll likely find me engaged in various sports activities, whether it's holding a racket, bat, paddle, or ball. I have a passion for sports and am particularly enthralled by cricket.</p>  
+        <p> When I'm not immersed in coding, you'll likely find me engaged in various sporting activities, whether it's holding a racket, bat, paddle, or ball. I have a passion for sports and am particularly enthralled by cricket.</p>  
         {/* Add nice picture here */}
     </motion.section>
   )
